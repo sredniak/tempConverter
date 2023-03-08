7f6025a0f2dbccfd98b6e811ffaf53d13e2067f8
sredniak <50489621+sredniak@users.noreply.github.com>
2023-03-08 01:51:28 +0100

    Commit initial files

---
+++ script.js
@@ -0,0 +1,58 @@
+const converter = document.querySelector('#converter')
+const result = document.querySelector('.result')
+const convBtn = document.querySelector('.conv')
+const resetBtn = document.querySelector('.reset')
+const changeBtn = document.querySelector('.change')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+
+let fahrenheit
+let celsius
+
+
+const changeVal = () => {
+    //jezeli span one ma wartość °C to zamien span one na °F  i span two na °C
+    if ( one.textContent === '°C') {
+        one.textContent = '°F'
+        two.textContent = '°C'
+        result.textContent = ''
+    } else {
+        one.textContent = '°C'
+        two.textContent = '°F'
+        result.textContent = ''
+    }
+}
+
+const fahrToCel = () => {
+    fahrenheit = converter.value * 1.8 + 32
+    result.textContent = `${converter.value} °C to ${fahrenheit.toFixed(1)} °F`
+    converter.value = ''
+}
+
+const celToFahr = () => {
+    celsius = (converter.value - 32) / 1.8
+    result.textContent = `${converter.value} °F to ${celsius.toFixed(1)} °C`
+converter.value = ''
+}
+
+const conversion = () => {
+    if(converter.value !== ''){
+        if(one.textContent === '°C'){
+            fahrToCel()
+        } else {
+            celToFahr()
+        }
+    } else {
+        result.textContent = 'Musisz podać jakąś wartość'
+    }
+}
+
+const reset = () => {
+    converter.value = ''
+    result.textContent = ''
+}
+
+
+changeBtn.addEventListener('click', changeVal)
+convBtn.addEventListener('click', conversion)
+resetBtn.addEventListener('click', reset)
